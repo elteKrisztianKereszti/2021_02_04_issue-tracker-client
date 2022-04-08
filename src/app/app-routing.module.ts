178fa822d1f107d8ff817fcda557a3627339014a
kereszti.krisztian <kereszti.krisztian@marktsoft.hu>
2022-04-08 13:49:11 +0200

    add routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,28 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { IssueDetailComponent } from './components/issue-detail/issue-detail.component';
+import { IssueFormComponent } from './components/issue-form/issue-form.component';
+import { IssueListComponent } from './components/issue-list/issue-list.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: "",
+    redirectTo: "/issues",
+    pathMatch: "full",
+  },
+  {
+    path: "issues",
+    component: IssueListComponent,
+  },
+  {
+    path: "issues/new",
+    component: IssueFormComponent,
+  },
+  {
+    path: "issues/:id",
+    component: IssueDetailComponent,
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],

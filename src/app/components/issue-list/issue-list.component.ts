88b3dcd02771b1415866544310cf2db1adcaeb62
kereszti.krisztian <kereszti.krisztian@marktsoft.hu>
2022-04-22 13:21:15 +0200

    add issue list dynamic

--- src/app/components/issue-list/issue-list.component.ts
+++ src/app/components/issue-list/issue-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Issue } from 'src/app/issue';
 
 @Component({
   selector: 'app-issue-list',
@@ -6,6 +7,37 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./issue-list.component.css']
 })
 export class IssueListComponent implements OnInit {
+
+  public issues: Array<Issue> = [
+    {
+      id : 1,
+      title: 'Bad Machine',
+      description: 'Donec id elit non mi porta gravida at eget metus. Maecenas sed diam egetrisus varius blandit.',
+      place: 'PC1',
+      status: 'NEW'
+    },
+    {
+      id : 2,
+      title: 'Bad Mosue',
+      description: 'Mickey Mouse destoyed the mouse',
+      place: 'PC2',
+      status: 'DOING'
+    },
+    {
+      id : 3,
+      title: 'Jim Raynor cannot play',
+      description: 'Jim Raynor cannot start the starcraft on 386 arch pc',
+      place: 'PC42',
+      status: 'NEW'
+    },
+    {
+      id : 4,
+      title: 'No power',
+      description: 'Summon more Pylon',
+      place: 'PC66',
+      status: 'DONE'
+    }
+  ];
 
   constructor() { }
 
--- src/app/issue.ts
+++ src/app/issue.ts
@@ -3,5 +3,5 @@ export class Issue {
   title: string = '';
   description: string = '';
   place: string = '';
-  state: string = '';
+  status: string = '';
 }

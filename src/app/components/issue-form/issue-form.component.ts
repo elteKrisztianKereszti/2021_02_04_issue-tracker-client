993ffb47e4a2730a20769d83786e9ab523b86b66
kereszti.krisztian <kereszti.krisztian@marktsoft.hu>
2022-04-29 13:24:04 +0200

    add isinvalid classes

--- src/app/components/issue-form/issue-form.component.ts
+++ src/app/components/issue-form/issue-form.component.ts
@@ -1,4 +1,5 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, Input, OnChanges, OnInit, SimpleChanges } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { Issue } from 'src/app/issue';
 
 @Component({
@@ -6,12 +7,31 @@ import { Issue } from 'src/app/issue';
   templateUrl: './issue-form.component.html',
   styleUrls: ['./issue-form.component.css']
 })
-export class IssueFormComponent implements OnInit {
+export class IssueFormComponent implements OnInit, OnChanges {
   @Input() issue: Issue;
 
-  constructor() { }
+  public issueForm: FormGroup;
 
-  ngOnInit(): void {
+  constructor(private formBuilder: FormBuilder) { }
+
+  public ngOnInit(): void {
+    console.log('init');
   }
 
+  public ngOnChanges(changes: SimpleChanges): void {
+    this.issueForm = this.formBuilder.group({
+      title: [ this.issue.title, [ Validators.required ]],
+      description: this.issue.description,
+      place: [ this.issue.place, [ Validators.required, Validators.pattern(/^PC\d+$/) ]],
+      status: [ this.issue.status, [ Validators.required ]],
+    });
+  }
+
+  public onSave(e: any) {
+    console.log(this.issueForm);
+  }
+
+  public isInvalidAndTouchedOrDirty(name: string) {
+    return !this.issueForm.get(name)?.valid && (this.issueForm.get(name)?.touched || this.issueForm.get(name)?.dirty)
+  }
 }

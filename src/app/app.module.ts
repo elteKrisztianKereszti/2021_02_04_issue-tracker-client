ad804b4930ed30827cedbb693ec98b2f580f9244
kereszti.krisztian <kereszti.krisztian@marktsoft.hu>
2022-05-12 22:29:30 +0200

    remove unused components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,12 +4,10 @@ import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './components/app/app.component';
-import { MyComponentComponent } from './components/my-component/my-component.component';
 import { IssueListComponent } from './components/issue-list/issue-list.component';
 import { IssueDetailComponent } from './components/issue-detail/issue-detail.component';
 import { IssueFormComponent } from './components/issue-form/issue-form.component';
 import { IndexComponent } from './components/index/index.component';
-import { GlinkComponent } from './components/glink/glink.component';
 import { StatusFilterComponent } from './components/status-filter/status-filter.component';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { IssueNewComponent } from './components/issue-new/issue-new.component';
@@ -17,12 +15,10 @@ import { IssueNewComponent } from './components/issue-new/issue-new.component';
 @NgModule({
   declarations: [
     AppComponent,
-    MyComponentComponent,
     IssueListComponent,
     IssueDetailComponent,
     IssueFormComponent,
     IndexComponent,
-    GlinkComponent,
     StatusFilterComponent,
     IssueNewComponent
   ],
